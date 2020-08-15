60cfa20a406a0b3da0e4a02bcee82ed1f32637d4
jiangzhi.xie <xiejiangzhi@gmail.com>
2020-08-15 20:19:49 +0800

    Better rotate_mat function

--- src/shader/shadow.glsl
+++ src/shader/shadow.glsl
@@ -1,42 +1,38 @@
 #pragma language glsl3
 
-mat4 rotate_mat(float angle, vec3 axis) {
-  float l = length(axis);
-	float x = axis.x / l, y = axis.y / l, z = axis.z / l;
-	float c = cos(angle);
-	float s = sin(angle);
-
-  return mat4(
-    x * x * (1 - c) + c, y * x * (1 - c) + z * s, x * z * (1 - c) - y * s, 0,
-    x * y * (1 - c) - z * s, y * y * (1 - c) + c, y * z * (1 - c) + x * s, 0,
-    x * z * (1 - c) + y * s, y * z * (1 - c) - x * s, z * z * (1 - c) + c, 0,
-    0, 0, 0, 1
-  );
-}
-
-mat4 transform_mat(vec3 angle, float scale) {
-  mat4 r = rotate_mat(angle.x, vec3(1, 0, 0))
-    * rotate_mat(angle.y, vec3(0, 1, 0))
-    * rotate_mat(angle.z, vec3(0, 0, 1));
-  mat4 s = mat4(
-    scale, 0, 0, 0,
-    0, scale, 0, 0,
-    0, 0, scale, 0,
-    0, 0, 0, 1
-  );
-
-  return s * r;
-}
-
-
 #ifdef VERTEX
+
 uniform mat4 projection_mat;
 uniform mat4 view_mat;
 uniform mat4 model_mat;
 
 attribute vec3 ModelPos;
 attribute vec3 ModelAngle;
-attribute float ModelScale;
+attribute vec3 ModelScale;
+
+mat3 rotate_mat(vec3 angle) {
+  float c1 = cos(angle.z);
+  float s1 = sin(angle.z);
+  float c2 = cos(angle.x);
+  float s2 = sin(angle.x);
+  float c3 = cos(angle.y);
+  float s3 = sin(angle.y);
+
+  return mat3(
+    c1 * c3 - s1 * s2 * s3, c3 * s1 + c1 * s2 * s3, -c2 * s3,
+    -c2 * s1, c1 * c2, s2,
+    c1 * s3 + c3 * s1 * s2, s1 * s3 - c1 * c3 * s2, c2 * c3
+  );
+}
+
+mat4 transform_mat(vec3 angle, vec3 scale) {
+  mat3 m = rotate_mat(angle) * mat3(
+    scale.x, 0, 0,
+    0, scale.y, 0,
+    0, 0, scale.z
+  );
+  return mat4(m);
+}
 
 vec4 position(mat4 transform_projection, vec4 vertex_position) {
   mat4 model_mat = transform_mat(ModelAngle, ModelScale);
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -19,32 +19,28 @@ attribute vec3 ModelScale;
 attribute vec4 ModelAlbedo;
 attribute vec4 ModelPhysics;
 
-mat4 rotate_mat(float angle, vec3 axis) {
-  float l = length(axis);
-	float x = axis.x / l, y = axis.y / l, z = axis.z / l;
-	float c = cos(angle);
-	float s = sin(angle);
+mat3 rotate_mat(vec3 angle) {
+  float c1 = cos(angle.z);
+  float s1 = sin(angle.z);
+  float c2 = cos(angle.x);
+  float s2 = sin(angle.x);
+  float c3 = cos(angle.y);
+  float s3 = sin(angle.y);
 
-  return mat4(
-    x * x * (1 - c) + c, y * x * (1 - c) + z * s, x * z * (1 - c) - y * s, 0,
-    x * y * (1 - c) - z * s, y * y * (1 - c) + c, y * z * (1 - c) + x * s, 0,
-    x * z * (1 - c) + y * s, y * z * (1 - c) - x * s, z * z * (1 - c) + c, 0,
-    0, 0, 0, 1
+  return mat3(
+    c1 * c3 - s1 * s2 * s3, c3 * s1 + c1 * s2 * s3, -c2 * s3,
+    -c2 * s1, c1 * c2, s2,
+    c1 * s3 + c3 * s1 * s2, s1 * s3 - c1 * c3 * s2, c2 * c3
   );
 }
 
 mat4 transform_mat(vec3 angle, vec3 scale) {
-  mat4 r = rotate_mat(angle.x, vec3(1, 0, 0))
-    * rotate_mat(angle.y, vec3(0, 1, 0))
-    * rotate_mat(angle.z, vec3(0, 0, 1));
-  mat4 s = mat4(
-    scale.x, 0, 0, 0,
-    0, scale.y, 0, 0,
-    0, 0, scale.z, 0,
-    0, 0, 0, 1
+  mat3 m = rotate_mat(angle) * mat3(
+    scale.x, 0, 0,
+    0, scale.y, 0,
+    0, 0, scale.z
   );
-
-  return s * r;
+  return mat4(m);
 }
 
 vec4 position(mat4 transform_projection, vec4 vertex_position) {
